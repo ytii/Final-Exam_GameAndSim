ef9fac8285287195979ab6fc2afe4d6dbd86c2e5
ytii <derekwaderobert@gmail.com>
2022-05-28 02:00:38 -0400

    added stuff

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    public void StartLevel()
-    {
-        SceneManager.LoadScene(1); // 1 being the build index of Level_1
-    }
-
     public void QuitGame()
     {
         Application.Quit(); // Quit
         Debug.Log("<color=red> Quitting </color>"); // Tell us in unity editor that the game is exiting so we don't have to build the game to test
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
     }
 }
--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -21,7 +21,7 @@ public class LevelChange : MonoBehaviour
         yield return new WaitForSecondsRealtime(LevelLoadDelay);
         Time.timeScale = 1.0f;
 
-        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(2);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // find active scene so we can advance by one after interaction
+        SceneManager.LoadScene(currentSceneIndex + 1); // current scene +1 based on build index
     }
 }
